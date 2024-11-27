3dbc084cdc7117e43f456a125ea296c26445f16b
yjkim <youngggoong2016@gmail.com>
2024-11-27 13:23:22 +0900

    Initial commit

---
+++ api/src/main/java/com/example/AutoSystem/AutoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.AutoSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoSystemApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/example/AutoSystem/AutoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AutoSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
